ae36f9bb918cb9adfbae73eec829323bfbb38e44
slawick12 <yaschenko.slawick12@gmail.com>
2020-03-09 20:37:56 +0100

    I almost got it

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,30 @@
 $(function () {
-    $('.menu-btn').on('click', function () {
-        $('.header__menu').toggleClass('active')
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.png" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__rigth" src="img/arrow-right.png" alt="">',
+        speed: 900,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
     });
 
+    $('.menu-btn').on('click', function () {
+        $('.header__menu').toggleClass('active');
+    });
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 200) {
+            $(".header-bar").addClass("fill-in");
+            $(".header-bar").removeClass("fill-out");
+        } else {
+            if ($(".header-bar").hasClass("fill-in")) {
+                $(".header-bar").addClass("fill-out");
+                $(".header-bar").removeClass("fill-in");
+            }
+
+        }
+    });
 });
